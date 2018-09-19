0e51fd8dea2062c5595c8575f1e0624588bf033f
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-09-19 16:45:42 +0800

    Minor update

--- software/Classifier.py
+++ software/Classifier.py
@@ -1,8 +1,12 @@
 from sklearn.externals import joblib
 
 class Classifier:
-	# The model we retrieved from the persistence layer.
-	model
-
 	def __init__(self, model_path):
+		print("Attempting to load the model from %s" % model_path)
 		self.model = joblib.load(model_path)
+		print("Successfully loaded the model %s" % self.model)
+
+	def predict(input):
+		return model.predict(input)
+
+my = Classifier("models/sample.pkl")
