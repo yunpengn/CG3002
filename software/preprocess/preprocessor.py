91fb6d3fce9514ab8d0755f5aaf009ce4522bf78
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-09-21 10:27:48 +0800

    Add raw data and script for raw processing

--- software/preprocess/preprocessor.py
+++ software/preprocess/preprocessor.py
@@ -15,6 +15,9 @@ class Preprocessor:
 		data_X = dataset[self.X_columns].copy()
 		data_y = dataset[self.y_column].copy()
 
+		return split(data_X, data_y, test_size = 0.2)
+
+	def split(data_X, data_y, test_size = 0.2):
 		return train_test_split(data_X, data_y, test_size = 0.2, random_state = 0)
 
 	def prepare_predict(self, data_path):
@@ -22,3 +25,37 @@ class Preprocessor:
 		dataset = panda.read_csv(data_path, sep = ', ')
 		# Gets the average of the columns we need.
 		return np.array([dataset[column].mean() for column in self.X_columns])
+
+	# We assume the raw data to be 100Hz.
+	def read_raw(self, data_path, X_columns, interval = 20, period = 50):
+		# First read the data from CSV file format to panda dataframe format.
+		dataset = panda.read_csv(data_path, sep = ',')
+
+		# Use a dict to store the input.
+		X = {}
+		# Use a dict to store the output (label).
+		y = {}
+
+		for column in X_columns:
+			X['mean_' + column] = []
+			X['std_' + column] = []
+			X['min_' + column] = []
+			X['max_' + column] = []
+
+		y[self.y_column] = []
+
+		# Process for every interval using overlapping frame technique
+		for start in range(0, len(dataset) - period, interval):
+			instance = {}
+
+			# Process the data for each column
+			for column in X_columns:
+				current = dataset[column][start:start + period]
+				X['mean_' + column].append(current.mean())
+				X['std_' + column].append(current.std())
+				X['min_' + column].append(current.min())
+				X['max_' + column].append(current.max())
+
+			y[self.y_column].append(dataset[self.y_column][start])
+
+		return panda.DataFrame(data = X), panda.DataFrame(data = y)
--- software/test.py
+++ software/test.py
@@ -7,33 +7,59 @@ from trainers.knn_trainer import KnnTrainer
 # Test for run or walk
 ########################################
 
-preprocessor = Preprocessor(['acceleration_x', 'acceleration_y', 'acceleration_z', 'wrist'], 'activity')
-X_train, X_test, y_train, y_test = preprocessor.prepare_train('data/run_or_walk.csv')
+# preprocessor = Preprocessor(['acceleration_x', 'acceleration_y', 'acceleration_z', 'wrist'], 'activity')
+# X_train, X_test, y_train, y_test = preprocessor.prepare_train('data/run_or_walk.csv')
+
+# trainer = SvmTrainer()
+# trainer.train(X_train, y_train)
+# trainer.evaluate(X_test, y_test)
+# trainer.save('models/sample_kaggle.pkl')
+
+# classifier = Classifier("models/sample_kaggle.pkl")
+
+# ########################################
+# # Test for stationary sample data
+# ########################################
+
+# preprocessor = Preprocessor(['BodyX', 'handAcclX', 'handAcclY', 'handAcclZ', 'legAcclY'], 'Voltage')
+# input = preprocessor.prepare_predict('data/stationary_sample.csv')
+# print(input)
+
+# ########################################
+# # Test for raw movement dataset
+# ########################################
+
+# preprocessor = Preprocessor(['x1', 'y1', 'z1', 'x2', 'y2', 'z2', 'x3', 'y3', 'z3'], 'class')
+# input = preprocessor.prepare_predict('data/raw_movement_dataset.csv')
+
+# trainer = KnnTrainer()
+# #Note: Can choose to call this inside the class itself
+# knn_value = trainer.find_best_knn_value()
+# trainer.train(X_train, y_train, knn_value)
+# trainer.evaluate(X_test, y_test)
+
+# ########################################
+# # Test for old data
+# ########################################
+
+# preprocessor = Preprocessor(['meanX', 'meanY', 'meanY'], 'label')
+# X_train, X_test, y_train, y_test = preprocessor.prepare_train('data/old_data.csv')
+# trainer = SvmTrainer()
+
+# trainer = SvmTrainer()
+# trainer.train(X_train, y_train)
+# trainer.evaluate(X_test, y_test)
+# trainer.save('models/old_sample.pkl')
+
+# classifier = Classifier("models/old_sample.pkl")
+
+########################################
+# Test for raw data sample
+########################################
+
+preprocessor = Preprocessor(['meanX', 'meanY', 'meanY'], 'label')
+X, y = preprocessor.read_raw('data/raw/old_raw_sample.csv', ['x', 'y', 'z'])
+# X_train, X_test, y_train, y_test = preprocessor.split(X, y)
 
 trainer = SvmTrainer()
-trainer.train(X_train, y_train)
-trainer.evaluate(X_test, y_test)
-trainer.save('models/sample_kaggle.pkl')
-
-classifier = Classifier("../models/sample_kaggle.pkl")
-
-########################################
-# Test for stationary sample data
-########################################
-
-preprocessor = Preprocessor(['BodyX', 'handAcclX', 'handAcclY', 'handAcclZ', 'legAcclY'], 'Voltage')
-input = preprocessor.prepare_predict('data/stationary_sample.csv')
-print(input)
-
-########################################
-# Test for raw movement dataset
-########################################
-
-preprocessor = Preprocessor(['x1', 'y1', 'z1', 'x2', 'y2', 'z2', 'x3', 'y3', 'z3'], 'class')
-input = preprocessor.prepare_predict('data/raw_movement_dataset.csv')
-
-trainer = KnnTrainer()
-#Note: Can choose to call this inside the class itself
-knn_value = trainer.find_best_knn_value()
-trainer.train(X_train, y_train, knn_value)
-trainer.evaluate(X_test, y_test)
+trainer.train(X, y)
