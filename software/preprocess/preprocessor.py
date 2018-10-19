f1681e6ac9001a2e7e5d96a8d18d3d36c4ee70e6
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-10-19 15:39:27 +0800

    Refactor preprocessor class

--- software/preprocess/preprocessor.py
+++ software/preprocess/preprocessor.py
@@ -4,26 +4,26 @@ from sklearn.model_selection import train_test_split
 
 
 class Preprocessor:
-    def __init__(self, X_columns, y_column):
-        self.X_columns = X_columns
+    def __init__(self, x_columns, y_column):
+        self.x_columns = x_columns
         self.y_column = y_column
 
-    def prepare_train(self, data_path, test_size=0.2):
-        # First read the data from CSV file format to panda dataframe format.
+    def prepare_train(self, data_path):
+        # First read the data from CSV file format to panda data-frame format.
         data_frame = panda.read_csv(data_path, sep=', ')
 
         # Then, only keep the relevant column
-        data_X = data_frame[self.X_columns].copy()
+        data_x = data_frame[self.x_columns].copy()
         data_y = data_frame[self.y_column].copy()
 
-        return train_test_split(data_X, data_y, test_size=test_size, random_state=0)
+        return Preprocessor.split(data_x, data_y)
 
     @staticmethod
-    def split(data_X, data_y):
-        return train_test_split(data_X, data_y)
+    def split(data_x, data_y, test_percent=0.2, random=0):
+        return train_test_split(data_x, data_y, test_size=test_percent, random_state=random)
 
     def prepare_predict(self, data_path):
-        # First read the data from CSV file format to panda dataframe format.
+        # First read the data from CSV file format to panda data-frame format.
         dataset = panda.read_csv(data_path, sep=', ')
         # Gets the average of the columns we need.
-        return np.array([dataset[column].mean() for column in self.X_columns])
+        return np.array([dataset[column].mean() for column in self.x_columns])
