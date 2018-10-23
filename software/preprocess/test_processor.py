358f71f47ab9ef3e83967a3a6f42ea3634106ccc
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-10-23 17:45:40 +0800

    Use nparray

--- software/preprocess/test_processor.py
+++ software/preprocess/test_processor.py
@@ -1,14 +1,14 @@
 from utils.data_client import DataClient
-
+import numpy as np
 
 class TestProcessor:
     def __init__(self, x_columns):
-        self.x_columns = set(x_columns)
+        self.x_columns = x_columns
         self.data_client = DataClient()
 
     def get_data(self):
         input_data = self.data_client.getMlData()
-        return {key: value for key, value in input_data.items() if key in self.x_columns}
+        return np.array([input_data[column_name] for column_name in self.x_columns])
 
     def send_result(self, prediction_output):
         return
