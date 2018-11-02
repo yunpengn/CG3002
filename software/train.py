33d74f76a1b48bdef362836b7be5fd9250de71d8
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-11-02 16:21:32 +0800

    Add more classes

--- software/train.py
+++ software/train.py
@@ -9,7 +9,8 @@ x_columns = ["mean_handAcclX", "mean_handAcclY", "mean_handAcclZ",
 y_column = "label"
 processor = TrainProcessor(x_columns, y_column)
 
-all_paths = ["chicken.csv", "number7.csv", "sidestep.csv", "turnclap.csv", "wipers.csv", "stationary.csv"]
+classes = ["chicken.csv", "number7.csv", "sidestep.csv", "turnclap.csv", "wipers.csv", "stationary.csv",
+		   "cowboy.csv", "mermaid.csv", "number6.csv", "salute.csv", "swing.csv"]
 x_train, x_test, y_train, y_test = processor.prepare_train_all("data/extract", all_paths)
 
 trainer = RandomForestTrainer(14)
