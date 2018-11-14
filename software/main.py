f7c5a2b707ae7883d92a9891e5933fe604f7cdef
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-11-14 14:43:57 +0800

    Update threshold for chicken and logout

--- software/main.py
+++ software/main.py
@@ -12,8 +12,8 @@ if __name__ != '__main__':
 classifier = Classifier("models/random_forest.pkl")
 
 # Instantiates a result accumulator.
-classes = {"chicken": 6, "number7": 11, "sidestep": 10, "turnclap": 4, "wipers": 5, "stationary": 5,
-           "cowboy": 10, "mermaid": 13, "numbersix": 10, "salute": 10, "swing": 7, "logout": 18}
+classes = {"chicken": 5, "number7": 11, "sidestep": 10, "turnclap": 4, "wipers": 5, "stationary": 5,
+           "cowboy": 10, "mermaid": 13, "numbersix": 10, "salute": 10, "swing": 7, "logout": 14}
 accumulator = ResultAccumulator(classes)
 
 # Creates a processor for input data.
